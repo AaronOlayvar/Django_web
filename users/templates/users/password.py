216fa5f0fed1436930f0b716bd64e6df24fb8a0f
AaronOlayvar <trixolayvar@gmail.com>
2022-10-28 18:04:12 +0100

    Website code

---
+++ users/templates/users/password.py
@@ -0,0 +1,14 @@
+
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+special ="!@£$%^&*"
+
+used_for = lower_case + upper_case + numbers + special
+pass_length = 8 
+
+gen_pass = "".join(random.sample(used_for, pass_length))
+
+print ("Your random generated password is",gen_pass)
